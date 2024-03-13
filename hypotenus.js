cd1b7718d83699d0873588e3f4b6e5a53c422244
Collins Omollo <109079688+loskiii@users.noreply.github.com>
2024-03-14 00:24:51 +0300

    Add files via upload

---
+++ hypotenus.js
@@ -0,0 +1,15 @@
+let a;
+let b;
+let c;
+
+a = window.prompt("Enter side A: ")
+a = Number(a)
+
+b = window.prompt("Enter side B: ")
+b = Number(b)
+
+c = Math.pow(a,2) + Math.pow(b,2);
+c = Math.sqrt(c)
+
+console.log(c)
+
