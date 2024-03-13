313aacd0bde51a84f4af8a8210abf75e98a52956
Collins Omollo <109079688+loskiii@users.noreply.github.com>
2024-03-14 00:23:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+/*console.log('I like chicken');
+console.log('I am good');
+
+
+let firstName = "Collins";
+let age = 20;
+let student = true;
+
+console.log( "Hello" , firstName)
+console.log(age)
+console.log(student)
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age;
+document.getElementById("p3").innerHTML = "Enrolled  " + student; jmmmmmmmmhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+*/
+/*let height;
+ document.getElementById("myButton").onclick = function(){
+    height = document.getElementById("Height1").value;
+    console.log(height)
+ } 
+ */
+/*let username = window.prompt("What is your name?")
+console.log(username)*/
+
+/*let age = "21";
+age = Number(age)
+console.log(age, typeof age)
+*/
+
+/*const PI = 3.14;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius");
+radius = Number(radius);
+circumference = 2 * PI * radius;
+console.log("The circumference is" , circumference)*/
+
+let age = "Loski"
+age = Number(age)
+console.log(age)
+    
+console.log(Math.pow(2,3))
